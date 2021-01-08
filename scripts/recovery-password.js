8ff8f5608058f1b55ae2350dcd5f47eeb6b6cfc1
Caio Daló <caio.dalo@hotmail.com>
2021-01-08 14:20:15 -0300

    Added responsiveness for all pages

--- scripts/recovery-password.js
+++ scripts/recovery-password.js
@@ -1,7 +1,23 @@
 function recoveryPassword() {
-    const popupRecovery = window.open('./pages/recovery-password.html', 'recovery', "width=350, height=450, left=" + (screen.width - 500) + ", top=" + (screen.height - 600) / 2, "scrollbars=no")
+    const popupRecovery = window.matchMedia("(max-width: 768px)")
+
+    if (popupRecovery.matches) {
+        window.location.href = '../pages/recovery-password.html'
+    } else {
+        window.open('../pages/recovery-password.html', 'recovery', "width=500, height=450, left=" + (screen.width - 500) + ", top=" + (screen.height - 600) / 2, "scrollbars=no")
+    }
 
     return popupRecovery
+}
+
+function successScreenSize(popupSuccess) {
+
+    if (popupSuccess.matches) {
+        document.location.href = './recover-success.html'
+        console.log('ok ok')
+    } else {
+        window.open('./recover-success.html', 'success', "width=500, height=450, left=" + (screen.width - 500) + ", top=" + (screen.height - 600) / 2, "scrollbars=no")
+    }
 }
 
 function validationEmail() {
@@ -11,10 +27,17 @@ function validationEmail() {
     if (email.value === "" | email.value.indexOf(simbolValidation) == -1) {
         window.alert('Insira um email válido!')
     } else {
-        (window.open('./recover-success.html', 'success', "width=350, height=450, left=" + (screen.width - 500) + ", top=" + (screen.height - 600) / 2, "scrollbars=no"))
+        const popupSuccess = window.matchMedia("(max-width: 768px)")
+
+        successScreenSize(popupSuccess)
+        popupSuccess.addEventListener(successScreenSize)
     }
 }
 
 function closePopup() {
     window.close()
 }
+
+
+recoveryPassword(popupRecovery)
+popupRecovery.addEventListener(popupRecovery)
